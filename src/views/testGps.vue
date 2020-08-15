4f56663858e6bf435500a8f17c47f47f75c1300e
kkrsky <>
2020-08-15 22:43:22 +0900

    add watch Gps sysytem for demo

--- src/views/testGps.vue
+++ src/views/testGps.vue
@@ -5,11 +5,20 @@
     <v-btn @click="tes2()">test</v-btn>
     <p>{{ currentPosition }}</p>
     <p>
-      this isDiagnostic <span>{{ isDiagnostic }}</span>
+      this isDiagnostic
+      <span>{{ isDiagnostic }}</span>
     </p>
     <p>
-      this isDiagnosti <span>{{ isDevice }}</span>
+      this isDiagnosti
+      <span>{{ isDevice }}</span>
     </p>
+
+    <v-btn @click="watchCurrentPosition()">watch gps</v-btn>
+    <v-btn @click="watchCurrentPositionEnd()">end watch gps</v-btn>
+    <p>watch gps</p>
+    <ul>
+      <li v-for="(item,index) in watchPosition" :key="index">{{item}}</li>
+    </ul>
   </div>
 </template>
 
@@ -20,10 +29,16 @@ export default {
   data() {
     return {
       currentPosition: [],
+      watchPosition: [],
+      watchId: "",
+      performance: 0,
     };
   },
   computed: {
     ...mapState("cds", ["isDevice", "isDiagnostic"]),
+    watchedPosition() {
+      // return ...this.watchPosition
+    },
   },
   created() {
     this.$store.dispatch("cds/checkState");
@@ -49,6 +64,42 @@ export default {
         },
         { enableHighAccuracy: true }
       );
+    },
+    watchCurrentPosition() {
+      console.log("start");
+      let onSuccess = (position) => {
+        let nowTime = performance.now();
+        let divTime = nowTime - this.performance;
+        this.performance = nowTime;
+
+        let location = [
+          divTime,
+          position.coords.latitude,
+          position.coords.longitude,
+        ];
+        this.watchPosition.push(location);
+      };
+      let onFailed = (error) => {
+        alert(
+          "code: " + error.code + "\n" + "message: " + error.message + "\n"
+        );
+      };
+      let options = {
+        maximumAge: 3000,
+        enableHighAccuracy: true,
+        timeout: 50000,
+      };
+
+      let watchId = navigator.geolocation.watchPosition(
+        onSuccess,
+        onFailed,
+        options
+      );
+      this.watchId = watchId;
+    },
+    watchCurrentPositionEnd() {
+      console.log("end");
+      navigator.geolocation.clearWatch(this.watchId);
     },
   },
 };
