7a77b9dd72e9467f07005f62e1502ddfdbad1bc1
kkrsky <>
2020-08-16 17:31:19 +0900

    add environmental property system for more secure using dotenv

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,10 @@
+const Dotenv = require("dotenv-webpack"); //APIなどの環境変数をローカルに保存、process.env.OOOで取得可能
+
 module.exports = {
-  "publicPath": "./",
-  "outputDir": "www",
-  "transpileDependencies": [
-    "vuetify"
-  ]
-}
+  publicPath: "./",
+  outputDir: "www",
+  transpileDependencies: ["vuetify"],
+  configureWebpack: {
+    plugins: [new Dotenv()],
+  },
+};
