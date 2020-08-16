9053db88c038c3ce6646482f799cf57e112bc201
kkrsky <>
2020-08-16 17:28:46 +0900

    add environmental property system for more secure using dotenv

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+//内部jsライブラリ読み込み
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
@@ -6,13 +7,23 @@ import vuetify from "./plugins/vuetify";
 import virtualCordova from "./virtualCordova.js";
 import tes from "./mixins/tes";
 
-Vue.config.productionTip = false;
-// Vue.mixin(tes);
-// virtualCordova.start();
+//外部jsライブラリ読み込み
+let script = document.createElement("script");
+script.src =
+  "https://maps.googleapis.com/maps/api/js?key=" +
+  process.env.APIKEY_GOOGLE_ANDROID;
+document.head.appendChild(script);
 
+//外部cssライブラリ読み込み
 import "vuetify/dist/vuetify.min.css";
 import "../node_modules/leaflet/dist/leaflet.css";
 
+//ライブラリ宣言
+Vue.config.productionTip = false;
+// Vue.mixin(tes);
+virtualCordova.start();
+
+//Vue初期化
 new Vue({
   router,
   store,
