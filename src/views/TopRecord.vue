7ecef6aa2816716c62a9c7bb6c6ca80d8d17e7dc
kkrsky <>
2020-08-22 16:21:18 +0900

    add each page of outline

--- src/views/TopRecord.vue
+++ src/views/TopRecord.vue
@@ -1,10 +1,40 @@
 <template>
-  <p>this is record</p>
+  <div id="topRecord">
+    <top-header
+      :title="title"
+      :left="leftBtn"
+      :right="rightBtnList"
+    ></top-header>
+    <v-main>this is {{ title }}</v-main>
+    <top-footer></top-footer>
+  </div>
 </template>
 
 <script>
-export default {};
+import TopHeader from "@/components/TopHeader.vue";
+import TopFooter from "@/components/TopFooter.vue";
+
+export default {
+  data() {
+    return {
+      //component data
+      title: "record",
+      // rightBtnList: [],
+      // leftBtn: {},
+
+      //methods data
+    };
+  },
+  computed: {},
+  methods: {},
+  watch: {},
+  beforeCreate() {},
+  mounted() {},
+  components: {
+    TopHeader,
+    TopFooter,
+  },
+};
 </script>
 
-<style>
-</style>
+<style lang="scss"></style>
